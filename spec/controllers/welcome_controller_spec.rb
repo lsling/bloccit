24840012d9f30d5245cda1a7774d4a08209d6d93
Laura Slingerland <laura.sling@gmail.com>
2017-06-27 16:04:58 -0600

    Checkpoint 12 Bloccit: Completed

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+#1, we describe the subject of the spec, WelcomeController
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET index" do
+     it "renders the index template" do
+ # #2 use get to call the index method of WelcomeController
+       get :index
+ # #3 we expect the controller's response to render the index template
+       expect(response).to render_template("index")
+     end
+   end
+
+   describe "GET about" do
+     it "renders the about template" do
+       get :about
+       expect(response).to render_template("about")
+     end
+   end  
+end
