76283a7eba322ee7edbe2e67369d4847e9a6f551
Laura Slingerland <laura.sling@gmail.com>
2017-07-03 13:21:44 -0600

    Assignment 18 Bloccit: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   def index
     ## we declare an instance variable @posts and assign it a collection of Post objects using the all method provided by ActiveRecord
     @posts = Post.all
+    @posts.each_with_index do |k, v|
+      if v % 5 == 0
+        k.title = "SPAM"
+      end
+    end
   end
 
   def show
