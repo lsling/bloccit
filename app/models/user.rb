7febe402fabf33c1fa1ded400fee799807b00938
Laura Slingerland <laura.sling@gmail.com>
2017-07-09 16:14:46 -0600

    Checkpoint 23 Bloccit: Completed

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  ## we register an inline callback directly after the before_save callback
+  before_save { self.email = email.downcase if email.present? }
+  # # we use Ruby's validates function to ensure that name is present and has a maximum and minimum length
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  # # we validate password with two separate validations
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  # # we validate that email is present, unique, case insensitive, has a minimum length, has a maximum length, and that it is a properly formatted email address
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 254 }
+  # # we use Ruby's has_secure_password
+  has_secure_password
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
+  ## we create routes for new and create actions
+  resources :users, only: [:new, :create]
 
   ## we remove get "welcome/index" because we've declared the index view as the root view
   get 'about' => 'welcome#about'
