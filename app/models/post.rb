0cb7e9d997b138e8f56e41147e3d0a40c5e08c63
Laura Slingerland <laura.sling@gmail.com>
2017-07-17 23:06:02 -0600

    Checkpoint 30 Bloccit: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,32 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
 
+  def up_votes
+ # # we find the up votes for a post by passing value: 1 to where
+     votes.where(value: 1).count
+   end
+
+   def down_votes
+ # # we find the down votes for a post by passing value: -1 to where
+     votes.where(value: -1).count
+   end
+
+   def points
+ # # we use ActiveRecord's sum method to add the value of all the given post's votes
+     votes.sum(:value)
+   end
+   def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+     new_rank = points + age_in_days
+     update_attribute(:rank, new_rank)
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,16 @@ topics = Topic.all
 # Create Posts
 50.times do
 # #1 Adding a ! instructs the method to raise an error if there's a problem with the data we're seeding. Using create without a bang could fail without warning, causing the error to surface later
-  Post.create!(
+  post = Post.create!(
 # #2 we use methods from a class that does not exist yet, RandomData, that will create random strings for title and body
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 posts = Post.all
@@ -65,3 +68,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
