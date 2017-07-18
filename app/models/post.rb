e30398d36a5b9dce4176a87bdc91e7a0cc9e6db4
Laura Slingerland <laura.sling@gmail.com>
2017-07-17 23:33:44 -0600

    Assignment 30 Bloccit: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -25,9 +27,16 @@ class Post < ActiveRecord::Base
  # # we use ActiveRecord's sum method to add the value of all the given post's votes
      votes.sum(:value)
    end
+
    def update_rank
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   private
+
+   def create_vote
+     user.votes.create(value: 1, post: self)
    end
 end
